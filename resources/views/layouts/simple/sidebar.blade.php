422852d1a9240751ceab769e3e4a1b763ae81b2b
mannopovrustam <mannopovr@gmail.com>
2022-08-08 14:24:47 +0500

    breeze & livewire

---
+++ resources/views/layouts/simple/sidebar.blade.php
@@ -0,0 +1,61 @@
+<div class="vertical-menu">
+
+    <button type="button" class="btn btn-sm px-3 font-size-16 header-item waves-effect vertical-menu-btn">
+        <i class="fa fa-fw fa-bars"></i>
+    </button>
+
+    <div data-simplebar class="sidebar-menu-scroll">
+
+        <!--- Sidemenu -->
+        <div id="sidebar-menu">
+            <!-- Left Menu Start -->
+            <ul class="metismenu list-unstyled" id="side-menu">
+                <li class="menu-title">Menu</li>
+                <li>
+                    <a href="/projects">
+                        <i class="fa fa-building"></i>
+                        <span>Проекты</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="/project_properties">
+                        <i class="fa fa-boxes"></i>
+                        <span>Проект контент</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="/project_property_items">
+                        <i class="fa fa-box"></i>
+                        <span>Проект контент статии</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="/users">
+                        <i class="fa fa-user"></i>
+                        <span>Пользователи</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="/reports">
+                        <i class="fa fa-sticky-note"></i>
+                        <span>Новости</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="/employees">
+                        <i class="fa fa-user-tie"></i>
+                        <span>Работники</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="/messages">
+                        <i class="fa fa-envelope"></i>
+                        <span>Сообшении</span>
+                    </a>
+                </li>
+            </ul>
+
+        </div>
+        <!-- Sidebar -->
+    </div>
+</div>
